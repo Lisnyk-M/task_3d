67883cef1fbd25647d3596e330db6f3638fa598d
kolya lisnyk <swapbuffers@gmail.com>
2020-01-11 16:15:02 +0200

    added new files

---
+++ js/functions.js
@@ -0,0 +1,279 @@
+function parse_str(str){
+    var faces = []; 
+    var vertices = [];         
+    var count_n = 0;
+    var count_m = 0;
+    var data_f = str.split("s off");
+    
+    var v2 = [];
+    var f2 = [];
+    var str3 = '';
+    var arV = [];
+    var arTmp = [];
+    
+    var str2 = data_f[0]; //data_f[0].replace(/\s/g, '');
+    v2 = data_f[0].split('v');
+    
+    for (count_n = 1; count_n < v2.length ; count_n++){
+        arV = v2[count_n].split(' ');
+        arTmp[0] = parseFloat (arV[1]);
+        arTmp[1] = parseFloat (arV[2]);
+        arTmp[2] = parseFloat (arV[3]);
+        vertices.push(arTmp[0] ,arTmp[1]/*, arTmp[2]*/);    //x, y, z - coord              
+    }
+
+    f2 = data_f[1].split('f');        
+    var vec4 = [];
+    var tmpl = [];
+    var pess = new Object;
+    
+    for (count_m = 1; count_m < f2.length; count_m++){            
+        tmpl = f2[count_m].split(' '); 
+        pess.a = parseFloat(tmpl.pop());  
+        pess.b = parseFloat(tmpl.pop());
+        pess.c = parseFloat(tmpl.pop());
+        pess.d = parseFloat(tmpl.pop()); 
+        faces.push(pess.d, pess.c, pess.b, pess.a);              
+    }
+    var objReturn = new Object;
+    objReturn.vertices = vertices;
+    objReturn.faces = faces;
+    return objReturn;
+}
+//==================================================================================
+function getIndexesPoligon(vrtsX, vrtsY){ 
+    var gipn = 0;  
+    function gipIndexOf(arr, num){        
+        for (gipn = 0; gipn < 4; gipn++){                            
+            if(num == arr[gipn]){
+                // console.log(gipn, 'pes');
+                return gipn;
+                // break;
+            }else if (gipn == 3)
+                return -1; 
+        }           
+    }
+    function isParalel(arr1, arr2){
+        var ipIndex = [];
+        var ipT = arr1[0];
+        var ipN = 0;
+        for (var ipCx = 1; ipCx < 4; ipCx++){           
+            if (Math.max(Math.abs(arr1[ipCx]), Math.abs(ipT)) - Math.min(Math.abs(arr1[ipCx]), Math.abs(ipT)) < 0.0001){           
+                ipN++;
+                ipIndex[0] = arr1[ipCx];
+                ipIndex[1] = arr1[0];
+            }              
+        }
+        // console.log(ipN, arr1, Math.abs(ipT));
+        if (ipN > 0) 
+            return true;    //paralel polygon
+        else{
+            ipT = arr2[0];
+            for (var ipCx = 1; ipCx < 4; ipCx++){           
+                if (Math.max(Math.abs(arr2[ipCx]), Math.abs(ipT)) - Math.min(Math.abs(arr2[ipCx]), Math.abs(ipT)) < 0.0001){ 
+                    ipN++;
+                }
+            }
+            if (ipN > 0) 
+                return true;    //paralel polygon
+        }
+        return false;                    
+    }
+
+    var p = new Object;
+    var vertex0 = new Object;
+    var vertex1 = new Object;
+    var vertex2 = new Object;
+    var vertex3 = new Object;
+
+    var index = new Object;
+    p.vertex0 = vertex0;
+    p.vertex1 = vertex1;
+    p.vertex2 = vertex2;
+    p.vertex3 = vertex3;    
+    p.arrayOut = [0, 0, 0, 0];
+    var ktx = [9.9, 11.4, 7.5, 8];
+
+    if (!isParalel(vrtsX, vrtsY)){
+        p.vertex0.x = (Math.min (vrtsX[0], vrtsX[1], vrtsX[2], vrtsX[3]));
+        p.vertex2.x = (Math.max (vrtsX[0], vrtsX[1], vrtsX[2], vrtsX[3]));
+        p.vertex3.y = (Math.min (vrtsY[0], vrtsY[1], vrtsY[2], vrtsY[3]));
+        p.vertex1.y = (Math.max (vrtsY[0], vrtsY[1], vrtsY[2], vrtsY[3]));
+        
+        p.vertex0.index = gipIndexOf(vrtsX, p.vertex0.x);
+        p.vertex1.index = gipIndexOf(vrtsY, p.vertex1.y);
+        p.vertex2.index = gipIndexOf(vrtsX, p.vertex2.x);
+        p.vertex3.index = gipIndexOf(vrtsY, p.vertex3.y);
+        var gggg = Math.max (ktx[0], ktx[1], ktx[2], ktx[3]);
+        var hhh = gipIndexOf([5, 7, 9, 3], 3.9);
+        p.arrayOut[0] = p.vertex0.index;
+        p.arrayOut[1] = p.vertex1.index;
+        p.arrayOut[2] = p.vertex2.index;
+        p.arrayOut[3] = p.vertex3.index;        
+        p.arrayIndexes = p.arrayOut;
+    } else {
+        // console.log('primitive is perallel');
+        p.arrayOut[0] = 0;
+        p.arrayOut[1] = 1;
+        p.arrayOut[2] = 2;
+        p.arrayOut[3] = 3;  
+        p.arrayIndexes = p.arrayOut;       
+    }        
+    return p;
+}
+//==================================================================================
+function formingData(inV, inF){   //input array vertices and array faces; output two arrays [4] and [4]
+    var finX0, finX1, finX2, finX3;
+    var finY0, finY1, finY2, finY3;
+    var containX = [];
+    var containY = [];
+
+    for (var tmpw = 0; tmpw < inF.length / 4; tmpw++){
+        finX0 = inV[inF[tmpw*4 + 0]*2 -2];
+        finX1 = inV[inF[tmpw*4 + 1]*2 -2];
+        finX2 = inV[inF[tmpw*4 + 2]*2 -2];
+        finX3 = inV[inF[tmpw*4 + 3]*2 -2];
+
+        finY0 = inV[inF[tmpw*4 + 0]*2 -1];
+        finY1 = inV[inF[tmpw*4 + 1]*2 -1];
+        finY2 = inV[inF[tmpw*4 + 2]*2 -1];
+        finY3 = inV[inF[tmpw*4 + 3]*2 -1];     
+        var finArrayX = [];
+        var finArrayY = [];
+        finArrayX[0] = finX0;
+        finArrayX[1] = finX1;
+        finArrayX[2] = finX2;
+        finArrayX[3] = finX3;
+
+        finArrayY[0] = finY0;
+        finArrayY[1] = finY1;
+        finArrayY[2] = finY2;
+        finArrayY[3] = finY3; 
+        //---------------------------------------
+        //перестановка порядку вершин
+        var tmpx = [];
+        var tmpy = [];
+        var indexp = getIndexesPoligon(finArrayX, finArrayY);        
+        for (fdn = 0; fdn < 4; fdn ++){             
+            tmpx[fdn] = finArrayX[indexp.arrayIndexes[fdn]];
+            tmpy[fdn] = finArrayY[indexp.arrayIndexes[fdn]];
+        }
+        finArrayX = tmpx;
+        finArrayY = tmpy;
+        // console.log('indexp = ', indexp.arrayIndexes);
+        //--------------------------------------        
+        containY.push(finArrayY);
+        containX.push(finArrayX);
+    }            
+    // console.log('containX = ', containX.length);
+    var outPut = new Object;
+    outPut.containX = containX;
+    outPut.containY = containY;
+    outPut.finArrayX = finArrayX;
+    outPut.finArrayY = finArrayY;
+    return outPut;
+}
+//================================================================
+    function createPhisObjects(/*width, height, angle, */ar1, scale){
+        var arPhisObjs = [];
+        var obj = [];
+        for(var n = 0; n < ar1.length; n++){
+            obj = ar1[n];
+            arPhisObjs[n] = {
+                type:'box',
+                size:[obj.polygonWidth, obj.polygonHeight, 1], // size of shape
+                pos:[obj.posX, obj.posY, 0], // start position in degree
+                rot:[0,0,obj.angle], // start rotation in degree
+                move:false, // dynamic or statique
+                density: 1,
+                friction: 0.2,  //0.2
+                restitution: 0.2,
+                belongsTo: 1, // The bits of the collision groups to which the shape belongs.
+                collidesWith: 0xffffffff // The bits of the collision groups with which the shape collides.                            
+            };
+        }
+        return arPhisObjs;
+    }
+//==========================================================
+    function createSceeneObjects(arrInput, scale, pObjs){
+        var obj = [];
+        var cg = [];
+        var ar2 = [];
+        var cubeM = new THREE.MeshLambertMaterial(
+            {color: 0xcc0000}); 
+        var arOut = [];
+        for (n = 0; n < arrInput.length; n++){
+            obj = arrInput[n];
+            ar2 = pObjs[n];
+            cg[n] = new THREE.CubeGeometry(obj.polygonWidth*scale, obj.polygonHeight*scale, 1);
+            arOut[n] = new THREE.Mesh(cg[n], cubeM); 
+            arOut[n].receiveShadow = true 
+            // scene.add(arOut[n]);
+            arOut[n].position.set(obj.posX * scale, obj.posY * scale, 1 * scale);
+        }  
+        return arOut; 
+    }
+//===========================================================
+   function OutDataForPhisics(ax, ay, ind){
+        var kitX = [];
+        var kitY = [];
+        var oData = [];
+        var polygonWidth = 0;
+        var polygonHeight = 0;
+        var angle = 0;
+        var posX = 0;
+        var posY = 0;
+        var m = 0;
+        var k = 0;
+        var l = 0;
+        var radius = 0;
+        var cosBeta = 0;
+        var sinBeta = 0;
+        var xk = [];
+      
+        for (var n = 0; n < ax.length; n ++){
+            kitX = ax[n];
+            kitY = ay[n];
+            // console.log('kitX = ', kitX);
+            polygonWidth = Math.sqrt( (kitX[2] - kitX[3])*(kitX[2] - kitX[3]) + (kitY[2] - kitY[3])*(kitY[2] - kitY[3]) );
+            polygonHeight = Math.sqrt( (kitX[1] - kitX[2])*(kitX[1] - kitX[2]) + (kitY[1] - kitY[2])*(kitY[1] - kitY[2]) );
+    
+
+            // angle = 180 * Math.asin((kitY[2] - kitY[3]) / polygonWidth) /Math.PI;            
+            radius = Math.sqrt( (kitX[1] - kitX[3])*(kitX[1] - kitX[3]) + (kitY[1] - kitY[3])*(kitY[1] - kitY[3]) )  / 2;
+            sinBeta = ((kitY[2] - kitY[3]) / polygonWidth);
+            beta =  Math.asin(sinBeta);
+            cosBeta = Math.cos(beta);
+            angle = beta * 180 / Math.PI;
+           
+            // console.log('radius = ', radius);
+            var sinGamma = (polygonHeight / 2 )/radius;
+            // var alpha = Math.asin(sinGamma) + beta;
+            var alpha = Math.asin(sinGamma)*180/Math.PI + beta*180/Math.PI;
+            var ms = radius * Math.sin(alpha*Math.PI/180);
+            var ns = radius * Math.cos(alpha*Math.PI/180);
+            // console.log('alpha = ', alpha, ' m = ', ms, 'n = ', ns);
+
+            posY = kitY[3] + ms;
+            posX = kitX[3] + ns;
+            posX = (kitX[0] + kitX[1] + kitX[2] + kitX[3])/4;
+            posY = (kitY[0] + kitY[1] + kitY[2] + kitY[3])/4;
+            
+            // console.log('cosBeta = ', cosBeta, ' PosX = ', posX, 'PosY = ', posY);
+            // console.log('angle = ', angle, ' width = ', polygonWidth, ' height = ', polygonHeight);
+            // console.log('sinBeta = ', ax.length);
+            if (polygonHeight > polygonWidth){
+                // console.log('Height > Width');
+                // angle += 270;
+            }
+              
+            xk[n] = new Object;
+            xk[n].polygonWidth = polygonWidth;
+            xk[n].polygonHeight = polygonHeight;
+            xk[n].posX = posX;
+            xk[n].posY = posY;
+            xk[n].angle = angle;    
+            oData.push(xk[n]);
+        }
+        return oData;
+    }
---
+++ js/main.js
@@ -0,0 +1,265 @@
+
+var scene = new THREE.Scene;
+var meshs = [];
+window.onload=function(){     
+    var ind = document.getElementsByClassName("indicate");
+    var x = 0, z = 0;
+	var vec3 = new THREE.Vector3;    
+	//scene.background = new THREE.Color( 0x000000, 0 );
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 10000);
+    var render = new THREE.WebGLRenderer({ alpha: true, transparent: true  } ); 	//{ antialias: true },       
+    render.setSize(window.innerWidth, window.innerHeight);
+    render.setClearColor(0xEEEEEE, 0.7);
+    render.shadowMap.enabled = true;
+    render.shadowMap.type = THREE.PCFSoftShadowMap;
+    document.body.appendChild(render.domElement);
+
+    var axes = new THREE.AxesHelper( 60 );
+    scene.add(axes);
+    
+    var turn = -1;
+    var tm = document.getElementsByClassName('btn');
+
+    var screen_size = new THREE.Vector2;
+    render.getDrawingBufferSize(screen_size);
+//==========================  ============================
+	var shaderMaterial = new THREE.ShaderMaterial( {
+		vertexShader: document.getElementById( 'vertexShader' ).textContent,
+		fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+    });    
+    
+    camera.position.set(15, 20, 40);
+    camera.lookAt(scene.position);
+	var amColor = "#faffe3";
+	var amLight = new THREE.AmbientLight(amColor, 0.9);
+	scene.add(amLight);
+
+	var light = new THREE.DirectionalLight(0xfff7e8, 1); 
+	scene.add(light);
+    light.position.y += 40;
+        
+    //Set up shadow properties for the light
+    var spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.position.set( -40, 60, -10 );
+    spotLight.castShadow = true;	
+	spotLight.shadow.mapSize.width = 2048;  // default
+	spotLight.shadow.mapSize.height = 2048; // default
+	spotLight.shadow.camera.near = 0.5;       // default
+	spotLight.shadow.camera.far = 500      // default       
+    scene.add(spotLight );
+
+    //------------------phisics-----------------------
+    world = new OIMO.World({ 
+        timestep: 1/60, 
+        iterations: 8, 
+        broadphase: 2, // 1 brute force, 2 sweep and prune, 3 volume tree
+        worldscale: 10, // scale full world 
+        random: true,  // randomize sample
+        info: false,   // calculate statistic or not
+        gravity: [0,-9.8,0] 
+    });
+
+    var floor_phs = world.add({ 
+        type:'box', // type of shape : sphere, box, cylinder 
+        size:[10,1,10], // size of shape
+        pos:[0,-15,0], // start position in degree
+        rot:[0,0,10], // start rotation in degree
+        move:false, // dynamic or statique
+        density: 1,
+        friction: 0.2,  //0.2
+        restitution: 0.2,
+        belongsTo: 1, // The bits of the collision groups to which the shape belongs.
+        collidesWith: 0xffffffff // The bits of the collision groups with which the shape collides.
+    });
+    //---------------------- front limit polygon------------------------
+    var frontLimit = world.add({ 
+        type:'box', // type of shape : sphere, box, cylinder 
+        size:[70,70,1], // size of shape
+        pos:[0,0,1], // start position in degree
+        rot:[0,0,10], // start rotation in degree
+        move:false, // dynamic or statique
+        density: 1,
+        friction: 0.2,  //0.2
+        restitution: 0.2,
+        belongsTo: 1, // The bits of the collision groups to which the shape belongs.
+        collidesWith: 0xffffffff // The bits of the collision groups with which the shape collides.
+    });
+    //---------------------- back limit polygon------------------------
+    var backLimit = world.add({ 
+        type:'box', // type of shape : sphere, box, cylinder 
+        size:[70,70,1], // size of shape
+        pos:[0,0,-1], // start position in degree
+        rot:[0,0,10], // start rotation in degree
+        move:false, // dynamic or statique
+        density: 1,
+        friction: 0.2,  //0.2
+        restitution: 0.2,
+        belongsTo: 1, // The bits of the collision groups to which the shape belongs.
+        collidesWith: 0xffffffff // The bits of the collision groups with which the shape collides.
+    });
+    
+    //==========================add phisics simulate spheres==============
+    var arSpheres = [];
+    var SIZE_SPHERES = 0.47;
+    for (var nsp = 0; nsp < 40; nsp ++){        
+        arSpheres[nsp] = world.add({ 
+            type:'sphere', // type of shape : sphere, box, cylinder 
+            size:[SIZE_SPHERES, SIZE_SPHERES, SIZE_SPHERES], // size of shape
+            pos:[ -5 + (nsp%10)*SIZE_SPHERES*2, 12.8 + 0.5*Math.floor(nsp/10),  0*Math.random()*0.01], // start position in degree
+            rot:[0,0,10], // start rotation in degree
+            move:true, // dynamic or statique
+            density: 1,
+            friction: 0.1,  //0.2
+            restitution: 0.2,
+            belongsTo: 1, // The bits of the collision groups to which the shape belongs.
+            collidesWith: 0xffffffff // The bits of the collision groups with which the shape collides.
+        });
+    }
+    // //=========================================================    
+    var str1 = document.getElementById ('lab').textContent;
+    var d = parse_str(str1);        
+    var res = formingData(d.vertices, d.faces);   
+    var odf = OutDataForPhisics(res.containX, res.containY);    
+    var obScale = 1;
+    var cpo = createPhisObjects(odf, obScale);    
+    var sceneObjects = createSceeneObjects(odf, obScale, cpo);
+
+    var namesObjects = [];  //ідентифікатори об'єктів 
+
+    for (var cnt = 0; cnt < cpo.length; cnt++){                      
+        namesObjects[cnt] = world.add(cpo[cnt]);
+        sceneObjects[cnt].position.copy(namesObjects[cnt].getPosition() );      
+        sceneObjects[cnt].quaternion.copy(namesObjects[cnt].getQuaternion() );
+        sceneObjects[cnt].receiveShadow = true;
+    }
+    //============================phisics geom============================
+    var scale = 1;
+    var box_floor_phis_geometry = new THREE.CubeGeometry(10, 1, 10);
+    var floor = new THREE.Mesh(box_floor_phis_geometry, shaderMaterial);
+    floor.scale.set(scale, scale, scale);
+    scene.add(floor);
+    floor.position.copy( floor_phs.getPosition() );
+    floor.quaternion.copy( floor_phs.getQuaternion() );
+
+    var arSpheresMesh = [];
+    var arGeom = [];
+    var sphM = new THREE.MeshPhongMaterial({color: 0x005500, /*side: THREE.DoubleSide, */wireframe: false});
+    for (var nn = 0; nn < arSpheres.length; nn++){
+        arGeom = new THREE.SphereBufferGeometry(SIZE_SPHERES, 10, 10);
+        arSpheresMesh[nn] = new THREE.Mesh(arGeom, sphM);
+        arSpheresMesh[nn].castShadow = true;
+        scene.add(arSpheresMesh[nn]);    
+    }
+    //============================end of phisics geom============================
+
+    meshes = [];
+    var objLoader = new THREE.OBJLoader();
+    objLoader.load('model/visual_model.obj', function(object){ 
+        object.traverse(function(child){
+            if (child instanceof THREE.Mesh){
+                meshes.push(child);
+            }
+        });
+		var body = meshes[0];   
+		body.scale.set(10, 10, 10);		
+		body.receiveShadow = true;
+		scene.add(body);      
+        body.material = new THREE.MeshPhongMaterial({  
+            color: 0xcc0000,                 
+            specular: 0x333333,                       
+        });      
+    });    
+    
+    var strela = new THREE.Mesh();
+    var strelkaLoader = new THREE.OBJLoader();
+    strelkaLoader.load('model/stralka.obj', function(object){ 
+        var meshes2 = [];
+        object.traverse(function(child){
+            if (child instanceof THREE.Mesh){
+                meshes2.push(child);
+            }
+        });               
+           
+        strela = meshes2[0];
+        strela.material = new THREE.MeshPhongMaterial({specular: 0x222222, color: 0x0000ff});
+        strela.castShadow = true;
+        scene.add(strela);
+        // console.log('Mesh strela = ', strela.geometry.attributes.position);
+    });
+    
+    var controls = new THREE.OrbitControls (camera, render.domElement);  
+	controls.enablePan = false;
+    // controls.autoRotate = true;
+    // controls.autoRotateSpeed = 300;
+    
+    var angle = 0;  
+    var angle_change = 2*Math.PI/180;
+    var k_left = false;
+    var k_right = false;
+    var k_up = false;
+    var k_down = false;
+     
+	document.addEventListener('keydown', function(event) {
+		if (event.key =="ArrowLeft")            
+            k_left = true;
+		if (event.key =="ArrowRight")
+            k_right = true;    
+		if (event.key =="ArrowUp"){
+            k_up = true;
+        }
+		if (event.key =="ArrowDown"){
+            k_down = true;
+        }        
+    });
+
+    document.addEventListener('keyup', function(event) {
+        if (event.key =="ArrowLeft")
+            k_left = false;
+        if (event.key =="ArrowRight")
+            k_right = false;        
+        if (event.key =="ArrowUp")
+            k_up = false;
+        if (event.key =="ArrowDown")
+            k_down = false;                      
+    });
+	
+    var rendering = function(){  
+//=======================key test====================
+        if (k_left)
+            angle += angle_change;
+            angle %= 2*Math.PI;
+        if (k_right)
+            angle -= angle_change;
+            angle %= 2*Math.PI;
+        if (k_up){
+            z -=0.2*Math.cos(angle);
+            x -=0.2*Math.sin(angle);        
+        }
+        if (k_down){
+            z +=0.2*Math.cos(angle);
+            x +=0.2*Math.sin(angle);
+        }
+//======================= end key test====================
+// update world
+world.step();
+
+for (var mp = 0; mp < arSpheres.length; mp++){
+    var mpos = arSpheres[mp].getPosition();
+    if (mpos.y < -30){
+        arSpheres[mp].resetPosition(0, 13, 0);
+    }
+    arSpheresMesh[mp].position.copy (arSpheres[mp].getPosition() );
+    arSpheresMesh[mp].quaternion.copy ( arSpheres[mp].getQuaternion() );    
+}
+//-----------------------end of phisics-------------------
+        strela.position.z = z + -20;
+        strela.position.x = x;
+        strela.rotation.y = angle;
+		// camera.getWorldDirection(vec3);
+        ind[0].innerHTML = 'x=' + x + '<br>' + 'z=' + z + '<br>' +'angle=' + angle*180/Math.PI + '<br>';
+        requestAnimationFrame(rendering);
+        controls.update();
+        render.render(scene, camera);        
+    }
+    rendering();
+}
